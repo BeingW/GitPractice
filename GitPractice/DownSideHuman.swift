81c07fab169a3c040e2a696c290cae8a14a79ec4
Jae900 <abraxas.jk@gmail.com>
2019-08-06 17:38:43 +0900

    Create DownSideHuman

---
+++ GitPractice/DownSideHuman.swift
@@ -0,0 +1,20 @@
+//
+//  DownSideHuman.swift
+//  GitPractice
+//
+//  Created by Insighter on 06/08/2019.
+//  Copyright © 2019 Insighter. All rights reserved.
+//
+
+import Foundation
+
+class DownSideHuman {
+    let leg: String
+    let foot: String
+    
+    init(leg: String, foot: String) {
+        self.leg = leg
+        self.foot = foot
+    }
+    
+}
